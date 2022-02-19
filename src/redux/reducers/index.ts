6e7833066b0113a0e6de3571d297aabb51c5b053
hemasali <ibraheems.ali95@gmail.com>
2022-02-19 22:14:50 +0200

    Products Reducer

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { OrdersReducer } from './ordersReducer';
+import { productsReducer } from './productsReducer';
 
 const reducers = combineReducers({
   OrdersReducer,
+  productsReducer,
 });
 
 export default reducers;
---
+++ src/redux/reducers/productsReducer.ts
@@ -0,0 +1,43 @@
+/* eslint-disable default-param-last */
+import { Action, Reducer } from 'redux';
+import {
+  ProductsActionTypes,
+  FETCH_PRODUCTS_START,
+  FETCH_PRODUCTS_FAILED,
+  FETCH_PRODUCTS_SUCCESS,
+} from '@/redux/actionTypes/products';
+import { ProductsState } from '@/types/productsTypes';
+
+const initialState: ProductsState = {
+  products: [],
+  loading: true,
+  error: null,
+};
+
+export const productsReducer: Reducer<ProductsState, Action> = (
+  state = initialState,
+  action: ProductsActionTypes,
+) => {
+  switch (action.type) {
+    case FETCH_PRODUCTS_START:
+      return {
+        ...state,
+        loading: true,
+      };
+    case FETCH_PRODUCTS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        products: action.payload,
+      };
+    case FETCH_PRODUCTS_FAILED: {
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    }
+    default:
+      return state;
+  }
+};
