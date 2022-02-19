4fbd93b3f860011d14dc8a5b99297a254b3b1e5a
hemasali <ibraheems.ali95@gmail.com>
2022-02-19 04:08:54 +0200

    Use redux-devtools-extension and fixing AppDispatch

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
-export const store = createStore(reducers, {}, applyMiddleware(thunk));
+export const store = createStore(reducers, {}, composeWithDevTools(applyMiddleware(thunk)));
 
 export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = ReturnType<typeof store.dispatch>['dispatch'];
