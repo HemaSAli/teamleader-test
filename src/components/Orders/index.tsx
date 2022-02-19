1ea5e01c9be7ea431626020b4dcb60402d31fdcc
hemasali <ibraheems.ali95@gmail.com>
2022-02-19 21:51:57 +0200

    Enhance code of index of orders page

--- src/components/Orders/index.tsx
+++ src/components/Orders/index.tsx
@@ -7,28 +7,33 @@ import OrderItem from './order';
 import './styles.css';
 
 function OrdersList() {
-  const {
-    OrdersReducer: { loading, orders },
-  } = useAppSelector((state) => state);
   const dispatch = useAppDispatch();
-
   useEffect(() => {
     dispatch(fetchOrders());
   }, [dispatch]);
 
+  const {
+    OrdersReducer: { loading, orders },
+  } = useAppSelector((state) => state);
+
+  if (loading) {
+    return (
+      <div className="teamleader-container">
+        <h3>Loading ...</h3>
+      </div>
+    );
+  }
+
   return (
     <div className="teamleader-container">
-      {!loading
-        ? (
-          <div className="orders-list">
-            <h3 className="orders-list-title">Orders List</h3>
-            <div className="orders-list-items">
-              {orders.map((order: Order) => (
-                <OrderItem order={order} />
-              ))}
-            </div>
-          </div>
-        ) : <h3>Loading ...</h3>}
+      <div className="orders-list">
+        <h3 className="orders-list-title">Orders List</h3>
+        <div className="orders-list-items">
+          {orders.map((order: Order) => (
+            <OrderItem order={order} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
