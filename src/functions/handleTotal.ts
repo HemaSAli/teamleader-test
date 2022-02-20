f1d37073bd997a0e11a7f6191e7f9dcf2eda51c2
hemasali <ibraheems.ali95@gmail.com>
2022-02-20 23:03:37 +0200

    Update helper function - add handleSub and handleAdd

--- src/functions/handleTotal.ts
+++ src/functions/handleTotal.ts
@@ -1,1 +1,9 @@
-export default (unitPrice: string, total: string) => `${(Number(unitPrice) + Number(total)).toFixed(2)}`;
+export const handleAdd = (unitPrice: string, total: string) => `${(Number(unitPrice) + Number(total)).toFixed(2)}`;
+export const handleSub = (
+  unitPrice: string,
+  total: string,
+  quantity: string = '1',
+) => {
+  const quantityMultiplieByUnitPrice = (Number(unitPrice) * Number(quantity)).toFixed(2);
+  return `${(Number(total) - Number(quantityMultiplieByUnitPrice)).toFixed(2)}`;
+};
