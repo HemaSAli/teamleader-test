673caa5ab0e110adcf9491da7d1a94f6d5a5b78f
hemasali <ibraheems.ali95@gmail.com>
2022-02-20 23:09:12 +0200

    Add link to orders page in home

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function HomePage() {
   return (
     <div className="teamleader-container">
-      <p>Hi =), Simple Home Page</p>
+      <Link to="/orders">
+        Hi =),
+        Click to go to orders page =)
+      </Link>
     </div>
   );
 }
